3a689397e2871b3f743da2dbd7a7ffdee336b7d5
yuki <yuki.onishi1129@gmail.com>
2022-10-13 17:03:38 +0900

    変数と関数

--- src/main/kotlin/Example.kt
+++ src/main/kotlin/Example.kt
@@ -1,3 +1,31 @@
 fun main() {
     println("Hello Kotlin.")
+
+//    // val: 変更不可
+//    val id = 100
+//    //    var: 変更可能
+//    var name = "takenaka"
+//
+//    println(id)
+//    println(name)
+
+//    val id: Int = 100
+//    var name: String = "takenaka"
+//    println(id)
+//    println(name)
+
+    println(countLength("yyyy"))
+
+    displayMessage("メッセージ")
+}
+
+// 関数
+fun countLength(str:String): Int {
+    return str.length
+}
+
+//　Unit: 「何もない」といいうことを表す型
+// 戻り値の型は省略可能
+fun displayMessage(message: String): Unit {
+    println(message)
 }
